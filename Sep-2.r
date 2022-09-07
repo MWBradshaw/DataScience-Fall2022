fb6e0b5bbe7db578df50367f8b2df2e5db7f7adf
MWBradshaw <mbradshaw@claflin.edu>
2022-09-07 14:38:34 -0400

    This commit will add Sep.7 to the repo

--- Sep-2.r
+++
@@ -1,7 +0,0 @@
-# Author: Bradshaw, Date: 09/01/22, Purpose: Calculator correlation analysis
-
-# Reading the data from csv
-setwd("/Users/matthewbradshaw/Desktop") # nolint
-data <- read.csv(file = "COVID-Data.csv", header=TRUE) # nolint
-
-head(data)
--- Sep-7.r
+++ Sep-7.r
@@ -1,3 +1,11 @@
+# Author: Bradshaw, Date: 09/01/22, Purpose: Calculator correlation analysis
+
+# Reading the data from csv
+setwd("/Users/matthewbradshaw/Desktop") # nolint
+data <- read.csv(file = "COVID-Data.csv", header=TRUE) # nolint
+
+head(data)
+
 # Correlation analysis between confirmed cases and number of deaths
 # Check if there is any relationship between number of deaths and confirmed cases 
 res <- cor.test(data$Confirmed, data$Deaths, method = "pearson"); 
